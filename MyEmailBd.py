e8e200386e616ac63b38d4be0cf0bf0fe2dbffbf
n3sky <162558241+n3sky0@users.noreply.github.com>
2024-04-09 19:08:40 +0700

    MyEmailBd.py
    I made a better conclusion and fixed the error when changing mail data

--- MyEmailBd.py
+++ MyEmailBd.py
@@ -62,43 +62,43 @@ def DeleteMail():
     
 def ChangeMail():
     email = input("Введите маил который хотите изменить: ")
-    if email != None and SearchUsers(email) == True:
+    if email!= None and SearchUsers(email) == True:
         connect = sqlite3.connect("emails.db")
         cur = connect.cursor()
         choice = int(input("Выберите что хотите изменить 1)Почта\n2)Имя\n3)Фамилия\n4)Дата рождения\n5)Пароль\n6)Дата создания\nНапишите цифру, которую хотите выбрать:"))
         if choice == 1:
             ChangeEmail = input("Введите емаил на который хотите поменять")
-            cur.execute("UPDATE emails SET email = ? WHERE email = ?", (ChangeEmail, email))
+            cur.execute("UPDATE emails SET email =? WHERE email =?", (ChangeEmail, email))
             connect.commit()
             connect.close()
             return True
         if choice == 2:
             ChangeName = input("Введите имя на которое хотите сменить")
-            cur.execute("UPDATE emails SET name = ? WHERE email = ?" (ChangeName, email))
+            cur.execute("UPDATE emails SET name =? WHERE email =?", (ChangeName, email))
             connect.commit()
             connect.close()
             return True
         if choice == 3:
             ChangeSurName = input("Введите фамилию на которое хотите сменить")
-            cur.execute("UPDATE emails SET surname = ? WHERE email = ?" (ChangeSurName, email))
+            cur.execute("UPDATE emails SET surname =? WHERE email =?", (ChangeSurName, email))
             connect.commit()
             connect.close()
             return True
         if choice == 4:
             ChangeBirthDay = input("Введите Дату рождения на которое хотите сменить")
-            cur.execute("UPDATE emails SET BirthDay = ? WHERE email = ?" (ChangeBirthDay, email))
+            cur.execute("UPDATE emails SET BirthDay =? WHERE email =?", (ChangeBirthDay, email))
             connect.commit()
             connect.close()
             return True
         if choice == 5:
             ChangePass = input("Введите пароль на которое хотите сменить")
-            cur.execute("UPDATE emails SET password = ? WHERE email = ?" (ChangePass, email))
+            cur.execute("UPDATE emails SET password =? WHERE email =?", (ChangePass, email))
             connect.commit()
             connect.close()
             return True
         if choice == 6:
             ChangeCreateDate = input("Введите дату создания на которое хотитеь сменить")
-            cur.execute("UPDATE emails SET create_date = ? WHERE email = ?" (ChangeCreateDate, email))
+            cur.execute("UPDATE emails SET create_date =? WHERE email =?", (ChangeCreateDate, email))
             connect.commit()
             connect.close()
             return True
@@ -113,7 +113,7 @@ def InputEmail():
     rows = cur.fetchall()
     
     for row in rows:
-        print(row)
+        print(f"Почта: {row[0]} ||| Имя: {row[1]} ||| Фамилия: {row[2]} ||| Дата рождения: {row[3]} ||| Пароль: {row[4]} ||| Дата создания: {row[5]}")
             
     connect.close()
 def main():
